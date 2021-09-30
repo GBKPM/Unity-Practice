8c48321261f803733e5a77ef502269a2897285e0
G-Nck <67905493+wlsdnvy@users.noreply.github.com>
2021-09-30 15:21:06 +0900

    cache clear

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float moveSpeed;
-    public float jumpForce;
-    public bool jumping = false;
+    [Header("Adjustments")]
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private float jumpForce;
+
+    [Header("Debug")]
+    [SerializeField]
+    private bool jumping = false;
+
+
 
     Rigidbody2D _rigidbody;
     SpriteRenderer spriteRenderer;
@@ -24,18 +32,20 @@ public class PlayerMove : MonoBehaviour
 
     void Update()
     {
-        float moveDis = moveSpeed * Time.deltaTime;
+        
         Vector3 inputDir = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetAxisRaw("Jump") == 1)
         {
             if (!jumping)
             {
                 jumping = true;
 
-                GetComponent<Rigidbody2D>().AddForce(Vector3.up * jumpForce);
+                _rigidbody.AddForce(Vector3.up * jumpForce);
             }
         }
+
+        float moveDis = moveSpeed * Time.deltaTime;
         transform.position += inputDir * moveDis;
         spriteRenderer.flipX = inputDir.y == 1;
 
@@ -43,7 +53,7 @@ public class PlayerMove : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         //바닥에 닿을때만 점프가 가능하도록
-        if(collision.gameObject.CompareTag("floor"))
+        if(collision.gameObject.CompareTag("Floor"))
         {
             jumping = false;
         }
