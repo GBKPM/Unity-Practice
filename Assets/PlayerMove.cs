e945acdc880e5068da507633363b236a48f1996e
GBKPM <s20024@gsm.hs.kr>
2021-10-01 15:23:42 +0900

    맵과 카메라 개편

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -65,7 +65,7 @@ public class PlayerMove : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.CompareTag("obstacle"))
+        if(collision.gameObject.CompareTag("obstacle") || collision.gameObject.CompareTag("Enemy"))
         {
             Destroy(this.gameObject);
         }
